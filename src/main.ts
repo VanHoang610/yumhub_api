67e8811430af4dc3541e800b0f05ea18f09b5886
Thi Trương <tr.minhthi98@gmail.com>
2024-05-21 12:52:50 +0700

    thi12:5221/5

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+
+  
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // cài đặt CORS
+  app.enableCors();
+  await app.listen(3001);
 
   if (module.hot) {
     module.hot.accept();
