0c538a6530fbce475538d59709bcca5cd41644a2
Thi Trương <tr.minhthi98@gmail.com>
2024-05-22 14:47:21 +0700

    thi22/5

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-
 declare const module: any;
 
 import { NestFactory } from '@nestjs/core';
@@ -8,10 +7,20 @@ import * as path from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Cấu hình CORS
+  app.enableCors({
+    origin: '*', // Hoặc bạn có thể chỉ định cụ thể các domain được phép
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
   app.useStaticAssets(path.join(__dirname, '..', 'uploads'), {
     prefix: '/uploads/',
   });
-  await app.listen(3000);
+  
+  await app.listen(3001);
 
   if (module.hot) {
     module.hot.accept();
