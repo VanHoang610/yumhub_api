f477a9007b5aa0b3a01a0a175e3bfae3b5a53082
thanhhoadev <doanthanhhoadev@gmail.com>
2024-06-17 09:21:10 +0700

    Use environment variable for Firebase Storage bucket

--- src/modules/upload/upload.service.ts
+++ src/modules/upload/upload.service.ts
@@ -1,38 +1,55 @@
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { Image } from 'src/schemas/image.schema';
+import * as admin from 'firebase-admin';
+import * as path from 'path';
 import * as fs from 'fs';
-import { promisify } from 'util';
-import * as path from 'path'; // Import path module
 
 @Injectable()
 export class UploadService {
-  constructor(@InjectModel('Image') private readonly imageModel: Model<Image>) {}
+  private bucket: admin.storage.Bucket;
+
+  constructor() {
+    const serviceAccount = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../auth/serviceAccountKey.json'), 'utf8'));
+
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,  // Thay bằng tên bucket của bạn
+    });
+
+    this.bucket = admin.storage().bucket();
+  }
 
   async uploadFile(file: Express.Multer.File): Promise<string> {
     if (!file) {
       throw new Error('No file uploaded');
     }
 
-    // Generate unique file name with original file extension
     const uniqueFileName = this.generateUniqueFileName(file.originalname);
+    const fileUpload = this.bucket.file(uniqueFileName);
 
-    // Construct full file path
-    const filePath = path.join(__dirname, '..', 'uploads', uniqueFileName);
+    const stream = fileUpload.createWriteStream({
+      metadata: {
+        contentType: file.mimetype,
+      },
+    });
 
-    // Move uploaded file to the designated directory
-    await promisify(fs.rename)(file.path, filePath);
+    return new Promise((resolve, reject) => {
+      stream.on('error', (error) => {
+        reject(error);
+      });
 
-    // Return the URL of the uploaded file
-    return 'http://192.168.0.102:3000/uploads/' + uniqueFileName;
+      stream.on('finish', () => {
+        fileUpload.makePublic().then(() => {
+          resolve(`https://storage.googleapis.com/${this.bucket.name}/${uniqueFileName}`);
+        });
+      });
+
+      stream.end(file.buffer);
+    });
   }
 
-  // Function to generate a unique file name by appending a timestamp
-  generateUniqueFileName(originalName: string): string {
+  private generateUniqueFileName(originalName: string): string {
     const timestamp = new Date().getTime();
-    const extension = path.extname(originalName); // Get file extension
-    return timestamp + extension; // Append timestamp to original file extension
+    const extension = path.extname(originalName);
+    return `${timestamp}${extension}`;
   }
 }
-
