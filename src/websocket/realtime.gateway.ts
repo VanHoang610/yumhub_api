fd26dbf919ac0962039ac31866d2e7ba07b3b38b
thanhhoadev <doanthanhhoadev@gmail.com>
2024-06-20 03:23:25 +0700

    ws 200624

--- src/websocket/realtime.gateway.ts
+++ src/websocket/realtime.gateway.ts
@@ -7,10 +7,20 @@ import {
   } from '@nestjs/websockets';
   import { Server, Socket } from 'socket.io';
   
+  interface ConnectedClient {
+    socket: Socket;
+    id_user: string;
+    type_user: string;
+  }
+  
   @WebSocketGateway()
   export class RealtimeGateway implements OnGatewayConnection, OnGatewayDisconnect {
     @WebSocketServer()
     server: Server;
+  
+    private customers: ConnectedClient[] = [];
+    private shippers: ConnectedClient[] = [];
+    private merchants: ConnectedClient[] = [];
   
     handleConnection(client: Socket) {
       console.log('Client connected:', client.id);
@@ -18,12 +28,30 @@ import {
   
     handleDisconnect(client: Socket) {
       console.log('Client disconnected:', client.id);
+      this.removeClient(client);
     }
   
-    @SubscribeMessage('message')
-    handleMessage(client: Socket, payload: any): void {
-      console.log('Received message:', payload);
-      this.server.emit('message', payload);
+    @SubscribeMessage('register')
+    handleRegister(client: Socket, payload: { id_user: string, type_user: string }) {
+      const { id_user, type_user } = payload;
+  
+      const connectedClient: ConnectedClient = { socket: client, id_user, type_user };
+      
+      if (type_user === 'customer') {
+        this.customers.push(connectedClient);
+      } else if (type_user === 'shipper') {
+        this.shippers.push(connectedClient);
+      } else if (type_user === 'merchant') {
+        this.merchants.push(connectedClient);
+      }
+  
+      console.log(`${type_user} registered:`, id_user);
+    }
+  
+    private removeClient(client: Socket) {
+      this.customers = this.customers.filter(c => c.socket.id !== client.id);
+      this.shippers = this.shippers.filter(s => s.socket.id !== client.id);
+      this.merchants = this.merchants.filter(m => m.socket.id !== client.id);
     }
   }
   
