5ce28c031774ec90ff5974b96822381858319f79
thanhhoadev <doanthanhhoadev@gmail.com>
2024-07-11 13:26:13 +0700

    feature stripe 8

--- src/modules/stripe/payout.controller.ts
+++ src/modules/stripe/payout.controller.ts
@@ -1,31 +1,13 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { PayoutService } from './payout.service';
 
+@Controller('payout')
+export class PayoutController {
+  constructor(private readonly payoutService: PayoutService) {}
 
-export class CreatePayoutDto {
-    amount: number;
-    bankAccountInfo: {
-      country: string;
-      currency: string;
-      account_id: string;
-      account_holder_name?: string;
-      account_holder_type?: string;
-      routing_number?: string;
-      account_number?: string;
-    };
+  @Post()
+  async createPayout(@Body() payoutDto: any): Promise<any> {
+    const { amount, bankAccountInfo } = payoutDto;
+    return this.payoutService.createPayout(amount, bankAccountInfo);
   }
-  
-  @Controller('payout')
-  export class PayoutController {
-    constructor(private readonly payoutService: PayoutService) {}
-  
-    @Post()
-    async createPayout(@Body() createPayoutDto: CreatePayoutDto): Promise<any> {
-      const { amount, bankAccountInfo } = createPayoutDto;
-      return this.payoutService.createPayout(amount, bankAccountInfo);
-    }
-    @Post('checkout')
-    async createCheckout(): Promise<any> {
-      return this.payoutService.createCheckout();
-    }
-  }
+}
--- src/modules/stripe/payout.service.ts
+++ src/modules/stripe/payout.service.ts
@@ -14,74 +14,26 @@ export class PayoutService {
 
   async createPayout(amount: number, bankAccountInfo: any): Promise<any> {
     try {
-      // Kiểm tra xem khách hàng đã tồn tại chưa
-      const customers = await this.stripe.customers.list({
-        email: bankAccountInfo.email,
-      });
-
-      let customer: Stripe.Customer;
-      if (customers.data.length === 0) {
-        // Nếu khách hàng chưa tồn tại, tạo mới khách hàng
-        customer = await this.stripe.customers.create({
-          email: bankAccountInfo.email,
-          name: bankAccountInfo.account_holder_name,
-        });
-      } else {
-        // Nếu khách hàng đã tồn tại, lấy thông tin khách hàng
-        customer = customers.data[0];
-      }
-
-      // Tạo phương thức thanh toán cho khách hàng
-      const paymentMethod = await this.stripe.paymentMethods.create({
-        type: 'card',
-        card: {
-          number: '4242424242424242',
-          exp_month: 8,
-          exp_year: 2026,
-          cvc: '314',
+      const bankAccountToken = await this.stripe.tokens.create({
+        bank_account: {
+          country: bankAccountInfo.country,
+          currency: bankAccountInfo.currency,
+          account_holder_name: bankAccountInfo.account_holder_name,
+          account_holder_type: bankAccountInfo.account_holder_type,
+          routing_number: bankAccountInfo.routing_number,
+          account_number: bankAccountInfo.account_number,
         },
       });
 
-      // Gắn phương thức thanh toán với khách hàng
-      await this.stripe.paymentMethods.attach(paymentMethod.id, {
-        customer: customer.id,
+      const payout = await this.stripe.payouts.create({
+        amount: amount,
+        currency: 'usd',
+        destination: bankAccountToken.id,
       });
 
-      // Đặt phương thức thanh toán mặc định cho khách hàng
-      await this.stripe.customers.update(customer.id, {
-        invoice_settings: {
-          default_payment_method: paymentMethod.id,
-        },
-      });
-
-      // Tạo thanh toán cho khách hàng
-      const paymentIntent = await this.stripe.paymentIntents.create({
-        amount: 500,
-        currency: 'gbp',
-        payment_method: 'pm_card_visa',
-      });
-
-      return paymentIntent;
+      return payout;
     } catch (error) {
-      throw error;
-    }
-  }
-  async createCheckout(): Promise<any> {
-
-    try {
-      const session = await this.stripe.checkout.sessions.create({
-        success_url: 'https://example.com/success',
-        line_items: [
-          {
-            price: 'price_1MotwRLkdIwHu7ixYcPLm5uZ',
-            quantity: 2,
-          },
-        ],
-        mode: 'payment',
-      });
-      return session
-    } catch (error) {
-      throw error;
+      throw new Error(`Error creating payout: ${error.message}`);
     }
   }
 }
